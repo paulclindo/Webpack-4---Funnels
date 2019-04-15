8b57fc065cd75721840ba3786c0f56369a6287bd
Paul Ccari <paulclindo@gmail.com>
2019-04-15 00:19:55 -0500

    Still missing Responsive Design

---
+++ webpack.dev.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const common = require('./webpack.common')
+const merge = require('webpack-merge')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = merge(common, {
+    mode: "development",
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        })
+    ],
+    module:{
+        rules: [
+            {
+                // test: /\.scss$/,
+                test: /\.s?css$/,
+                use: [
+                    'style-loader', //3.make styles inject into DOM
+                    'css-loader', //2.Turns css into common js
+                    'sass-loader' //1.Turns sass into css
+                ]
+            }
+        ]
+    }
+});
